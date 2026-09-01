7802d0291f12011884475e945eb0bad6000d79cd
murali11123 <muralivobillisetti62@gmail.com>
2026-09-01 19:54:14 +0530

    vite code updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,11 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-  }
-})
+    allowedHosts: ["cnc-designs.onrender.com"],
+  },
+  preview: {
+    port: 5173,
+    host: true,
+    allowedHosts: ["cnc-designs.onrender.com"],
+  },
+});
